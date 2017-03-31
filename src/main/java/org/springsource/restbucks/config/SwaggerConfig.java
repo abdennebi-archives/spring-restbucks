2f9e0401e90e50eb3d5dd00f89804fbc616831e4
Abdennebi Mohamed <abdennebi@gmail.com>
2017-04-01 01:34:23 +0200

    Add Swagger Support

---
+++ src/main/java/org/springsource/restbucks/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package org.springsource.restbucks.config;
+
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Import({BeanValidatorPluginsConfiguration.class})
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .build();
+    }
+}
